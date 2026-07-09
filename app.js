4abbeca2fdb9a4457ceaeaccbf87100200352eed
fujii-web <j2335@matsue-ct.ac.jp>
2026-07-09 06:38:30 +0000

    ストラテジーを設定しよう

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// authorization
+require("./config/passport")(app);
 // router
 app.use('/', require('./routes'));
 
